9cc1664b3b2788c65873cafaf4fa29c08cee57c1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-04 12:25:33 -0500

    crawling

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cypress-crawl-example
+
+> Crawling all local pages using Cypress example
+
+Cypress is not built to be a fast crawler, but for simple tests it will do. In this example, we crawl all pages reachable from the root page. See [spec.js](./cypress/integration/spec.js)
+
+![Crawling local pages](./images/crawl.gif)
+
+## To run
+
+```
+$ npm install
+$ npm run dev
+```
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,41 @@
 /// <reference types="cypress" />
 
-it('works', () => {})
+it('crawls all local pages', () => {
+  // let's keep track of all URLs we've visited
+  // NOTE: these URLs do not disambiguate between pages
+  // so if /about.html and /about are the same page,
+  // we will visit it twice if there are links to it
+  const visited = new Set()
+  const toVisit = ['/']
+
+  function visitNextUrl() {
+    if (toVisit.length === 0) {
+      return
+    }
+
+    const url = toVisit.pop()
+    if (visited.has(url)) {
+      return visitNextUrl()
+    }
+
+    visited.add(url)
+    cy.visit(url)
+
+    cy.get('a')
+      // there might not be any links on the page
+      // thus we need to ignore the built-in element check
+      .should(Cypress._.noop)
+      .then(($links) => {
+        const localUrls = $links
+          .toArray()
+          .map((link) => link.getAttribute('href'))
+          .filter((url) => !url.startsWith('http'))
+          .filter((url) => !visited.has(url))
+        cy.log(`found ${localUrls.length} new link(s) to visit`)
+        toVisit.push(...localUrls)
+      })
+      .then(visitNextUrl)
+  }
+
+  visitNextUrl()
+})
